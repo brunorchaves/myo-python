1c44564beb01148417c431240e28c651c2728712
Bruno Ribeiro <brunorchaves1@gmail.com>
2022-05-30 00:50:31 -0300

    recurrent nn model

--- gesture_predictor/trainGesture.py
+++ gesture_predictor/trainGesture.py
@@ -18,19 +18,36 @@ X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.75)
 print(X_train.shape, X_valid.shape)
 print(y)
 
+# label_mapping = {'A':0, 'B':1, 'C':2,'D':3, 'E':4}
+
 input_shape = (X.shape[1],)
 print(input_shape)
 
-model = keras.Sequential([
-    # the hidden ReLU layers
-    layers.BatchNormalization( input_shape= input_shape),
-    layers.Dense(800, activation='relu'),
-    layers.Dense(128, activation='relu'),
-    # layers.Dense(units=512, activation='relu'),
-    # the linear output layer 
-    layers.Dense(5, activation= "softmax"),
-])
 
+
+
+# RNN ***********************************************
+inputs = tf.keras.Input(shape=(X_train.shape[1],))
+expand_dims = tf.expand_dims(inputs, axis=2)
+gru = tf.keras.layers.GRU(256, return_sequences=True)(expand_dims)
+flatten = tf.keras.layers.Flatten()(gru)
+outputs = tf.keras.layers.Dense(5, activation='softmax')(flatten)
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+print(model.summary())
+
+
+# Normal NN **************************
+# model = keras.Sequential([
+#     # the hidden ReLU layers
+#     layers.BatchNormalization( input_shape= input_shape),
+#     layers.Dense(800, activation='relu'),
+#     layers.Dense(128, activation='relu'),
+#     # layers.Dense(units=512, activation='relu'),
+#     # the linear output layer 
+#     layers.Dense(5, activation= "softmax"),
+# ])
+
+# Model compile************************
 model.compile(
     optimizer='adam',
     loss='sparse_categorical_crossentropy',
@@ -39,7 +56,5 @@ model.compile(
 
 model.fit(X_train, y_train, epochs= 100)
 
-test_loss, test_acc = model.evaluate(X_valid,y_valid)
-
-print("Tested Acc:",test_acc)
-
+model_acc = model.evaluate(X_valid, y_valid, verbose=0)[1]
+print("Test Accuracy: {:.3f}%".format(model_acc * 100))
